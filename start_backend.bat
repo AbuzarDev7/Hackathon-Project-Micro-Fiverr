5ec7d531768368cb33997596a3f03c3a04898494
MuhammadAbbas-coder004 <your-email@gmail.com>
2026-04-12 20:43:54 +0500

    issue fix

---
+++ start_backend.bat
@@ -0,0 +1,6 @@
+@echo off
+echo Starting Micro-Fiverr Backend with PM2...
+cd /d "D:\Abuzar web and app developer\Hackathon-Project-Micro-Fiverr\back-end"
+pm2 resurrect
+echo Backend started! Check pm2 status with: pm2 status
+pause
